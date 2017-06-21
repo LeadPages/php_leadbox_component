becd125bbe8c8e0824a633099212a5ca7b30ebda
Tim Hart <tim.hart@ave81.com>
2017-06-21 08:24:02 -0500

    Cleanup

--- src/Leadboxes/Leadboxes.php
+++ src/Leadboxes/Leadboxes.php
@@ -30,32 +30,43 @@ class Leadboxes
     public $leadboxesUrl;
     public $certFile;
 
+   /**        
+    * @todo Add configuration/DI option for leadboxesUrl for testability        
+    * @todo Make certFile path configurable and optional        
+    *        
+    * @param \GuzzleHttp\Client  $client        
+    * @param LeadpagesLogin      $login         
+    *        
+    */
     public function __construct(Client $client, LeadpagesLogin $login)
     {
-
         $this->client = $client;
         $this->login = $login;
         $this->login->getApiKey();
         $this->leadboxesUrl = "https://api.leadpages.io/content/v1/leadboxes";
         $this->certFile = ABSPATH . WPINC . '/certificates/ca-bundle.crt';
-
     }
 
 
+    /**
+     * @return mixed|Exception
+     */
     public function getAllLeadboxes()
     {
         try {
-            $response = $this->client->get($this->leadboxesUrl,
-                [
-                    'headers' => ['Authorization' => 'Bearer ' . $this->login->apiKey],
-                    'verify' => $this->certFile,
-                ]);
+            $response = $this->client->get($this->leadboxesUrl, [
+                'headers' => ['Authorization' => 'Bearer ' . $this->login->apiKey],
+                'verify' => $this->certFile,
+            ]);
+
             $response = [
                 'code' => '200',
                 'response' => $response->getBody()->getContents()
             ];
+
         } catch (ClientException $e) {
             $response = $this->parseException($e);
+
         } catch (ConnectException $e) {
             $message = 'Can not connect to Leadpages Server:';
             $response = $this->parseException($e, $message);
@@ -64,17 +75,22 @@ class Leadboxes
         return $response;
     }
 
-
+   /**        
+    * Fetch leadbox embed code by id + type        
+    *        
+    * @param string $id        
+    * @param string $type        
+    *        
+    * @return mixed        
+    */
     public function getSingleLeadboxEmbedCode($id, $type)
     {
-
         try {
             $url = $this->buildSingleLeadboxUrl($id, $type);
-            $response = $this->client->get($url,
-                [
-                    'headers' => ['Authorization' => 'Bearer '. $this->login->apiKey],
-                    'verify' => $this->certFile,
-                ]);
+            $response = $this->client->get($url, [
+                'headers' => ['Authorization' => 'Bearer '. $this->login->apiKey],
+                'verify' => $this->certFile,
+            ]);
 
             $body = $response->getBody()->getContents();
             $body = json_decode($body, true);
@@ -83,10 +99,13 @@ class Leadboxes
                 'code' => '200',
                 'response' => json_encode(['embed_code' => $body['_items']['publish_settings']['embed_code']])
             ];
+
         } catch (ClientException $e) {
             $response = $this->parseException($e);
+
         } catch (ServerException $e) {
             $response = $this->parseException($e);
+
         } catch (ConnectException $e) {
             $message = 'Can not connect to Leadpages Server:';
             $response = $this->parseException($e, $message);
@@ -95,29 +114,36 @@ class Leadboxes
         return $response;
     }
 
+   /**        
+    *        
+    * @param string $id        
+    * @param string $type        
+    *        
+    * @return string Leadbox url        
+    */
     public function buildSingleLeadboxUrl($id, $type)
     {
         $queryParams = http_build_query(['popup_type' => $type]);
-        $url = $this->leadboxesUrl . '/' . $id . '?' . $queryParams;
-        return $url;
+        return $this->leadboxesUrl . '/' . $id . '?' . $queryParams;
     }
 
 
     /**
-     * @param $e
+     * Build response data structure for errors
      *
+     * @param Exception $e
      * @param string $message
      *
      * @return array
      */
     public function parseException($e, $message = '')
     {
-        $response = [
+        return [
             'code' => $e->getCode(),
             'response' => $message . ' ' . $e->getMessage(),
-            'error' => (bool)true
+            'error' => true
         ];
-        return $response;
     }
 
 }
+
